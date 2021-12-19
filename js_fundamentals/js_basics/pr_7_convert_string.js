e71fada03b317a87808648555422a4d98d5a2a7e
Rona <RonaHsu@gmail.com>
2021-12-19 08:53:46 -0600

    added problem 7

---
+++ js_fundamentals/js_basics/pr_7_convert_string.js
@@ -0,0 +1,35 @@
+// Convert a String to a Number
+// For this exercise we're going to learn more about type conversion by implementing our own parseInt function that converts a string of numeric characters (including an optional plus or minus sign) to a number.
+
+// The function takes a string of digits as an argument, and returns the appropriate number. Do not use any of the built-in functions for converting a string to a number type.
+
+// For now, do not worry about leading + or - signs, nor should you worry about invalid characters; assume all characters will be numeric.
+
+// Examples:
+
+const STRING_NUM = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '0': 0};
+
+function stringToInteger(string) {
+  
+  let value = 0;
+  const numbers = stringToNumbers(string);
+  
+  for (let i = 0; i < numbers.length; i += 1) {
+    value = 10 * value + numbers[i];
+  }
+  
+  return value;
+}
+
+function stringToNumbers(string) {
+  let result = [];
+  for (let i = 0; i < string.length; i++) {
+    result.push(STRING_NUM[string[i]]);
+  }
+  console.log(result);
+  return result;
+}
+
+console.log(stringToInteger('4321'));      // 4321
+console.log(stringToInteger('570'));       // 570
+
