3e1f74b9909e41606c93d27240b81fe852aecf77
Rona <RonaHsu@gmail.com>
2021-12-18 18:51:36 -0600

    added problem 6

--- js_fundamentals/js_basics/pr_6_counting_the_number.js
+++ js_fundamentals/js_basics/pr_6_counting_the_number.js
@@ -20,3 +20,14 @@ console.log(`There are ${a.length} characters in "${a}".`);
 let b = readlineSync.question('Please enter a phrase:');
 
 console.log(`There are ${b.length} characters in "${b}."`);
+
+// Further Exploration
+// The solution counts all the characters in the phrase, including spaces. Refactor it so that it ignores spaces.
+
+// Since the refactored program shouldn't count spaces, you can use the String.prototype.replace method to return a new string with all spaces removed from it. Go over the documentation to see how you can do this. The thing to note here is that the first argument passed to replace can be a regular expression pattern.
+
+// As an added challenge, further refactor the solution so that it only counts alphabetic characters.
+
+const readlineSync = require('readline-sync');
+let phrase = readlineSync.question('Please enter a phrase: ');
+console.log(`There are ${phrase.replace(/[^a-zA-Z0-9]/g, '').length} characters in "${phrase}".`);
